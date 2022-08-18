21a79bf932648d50ac7f2924ac911c68138e3859
Ayanda <68080779+mseshiAyanda@users.noreply.github.com>
2022-08-18 16:36:36 +0200

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from pypaystackapi.paystack import Transaction, Customer, Plan
+from paystackapi.paystack import Transaction, Customer, Plan
 from django.http import JsonResponse
 from django.conf import settings
 import json
